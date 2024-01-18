b6778e0cd4901e69cbb56ddeae9c33309f39b0e3
MateusBitar <129760150+MateusBitar@users.noreply.github.com>
2024-01-18 16:32:20 -0300

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import base64
+
+
+def image_to_base64(file):
+        with open(file, "rb") as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+    
+    
+img = image_to_base64("tenis.jpg")
+
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("data:image/pbg;base64,{img}");
+background-size: cover;
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html= True)
+st.title("Teste")
